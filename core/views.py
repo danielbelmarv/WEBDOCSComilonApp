0645aae99aaaeb5de691e849389e7f55a8c12760
danielbelmarv <danielbelmarv@gmail.com>
2022-06-18 19:47:43 -0400

    CasaReal

--- core/views.py
+++ core/views.py
@@ -1,11 +1,10 @@
 from django.http import Http404 
 from django.shortcuts import render, redirect, get_object_or_404
-from core.forms import CustomUserCreationForm, PlatoForm
+from core.forms import CustomUserCreationForm
 from core.models import Plato
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.core.paginator import Paginator
-from django.contrib.auth.decorators import login_required, permission_required
+
 
 # Create your views here.
 def index(request):
@@ -33,61 +32,3 @@ def registro(request):
         data['form'] = formulario
 
     return render(request, 'registration/registro.html', data)
-
-@permission_required('core.add_plato')
-def agregarPlato(request):
-
-    data = {
-        'form': PlatoForm()
-    }
-
-    if request.method == 'POST':
-        formulario = PlatoForm(data=request.POST, files=request.FILES)
-        if formulario.is_valid():
-            formulario.save()
-            data['mensaje'] = 'Guardado correctamente'
-        else:
-            data['form'] = formulario
-
-    return render(request, 'core/tools/agregar.html')
-
-@permission_required('core.view_plato')
-def listarPlatos(request):
-
-    platos = Plato.objects.all()
-    page = request.GET.get('page', 1)
-
-    try:
-        paginator = Paginator(platos, 5)
-        platos = paginator.page(page)
-    except:
-        raise Http404
-
-    data = {
-        'entity': platos,
-        'paginator': paginator
-    }
-
-    return render(request, 'core/tools/listar.html')
-
-@permission_required('core.change_producto')
-def modificarPlato(request, id):
-
-    plato = get_object_or_404(Plato, id=id)
-    data = {
-        'form': PlatoForm(instance=plato)
-    }
-
-    if request.method == 'POST':
-        formulario = PlatoForm(data=request.POST, instance=plato, files=request.FILES)
-        if formulario.is_valid():
-            formulario.save()
-            messages.success(request, 'Modificado correctamente')
-            return redirect(to='listar_platos')
-
-
-def eliminarPlato(request, id):
-    plato = get_object_or_404(Plato, id=id)
-    plato.delete()
-    messages.success(request, 'Eliminado correctamente')
-    return redirect(to='listar_platos')
